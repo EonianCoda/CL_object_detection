80c6744e438e0b78df8f84ac45ccd8f68c3faf63
EonianCoda <textuseful159000@gmail.com>
2021-07-12 01:14:58 +0800

    fix always augment bug

--- retinanet/dataloader.py
+++ retinanet/dataloader.py
@@ -353,21 +353,21 @@ class Augmenter(object):
 
     def __call__(self, sample, flip_x=0.5):
 
-        #if np.random.rand() < flip_x:
-        image, annots = sample['img'], sample['annot']
-        image = image[:, ::-1, :]
+        if np.random.rand() < flip_x:
+            image, annots = sample['img'], sample['annot']
+            image = image[:, ::-1, :]
 
-        rows, cols, channels = image.shape
+            rows, cols, channels = image.shape
 
-        x1 = annots[:, 0].copy()
-        x2 = annots[:, 2].copy()
+            x1 = annots[:, 0].copy()
+            x2 = annots[:, 2].copy()
 
-        x_tmp = x1.copy()
+            x_tmp = x1.copy()
 
-        annots[:, 0] = cols - x2
-        annots[:, 2] = cols - x_tmp
+            annots[:, 0] = cols - x2
+            annots[:, 2] = cols - x_tmp
 
-        sample = {'img': image, 'annot': annots}
+            sample = {'img': image, 'annot': annots}
 
         return sample
 
