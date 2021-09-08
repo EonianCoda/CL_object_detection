fix mix precision inf bug

train/train.py
@@ -18,7 +18,6 @@ def training_iteration(il_trainer:IL_Trainer, il_loss:IL_Loss, data, is_replay=F
         Args:
         Return: a dict, containing loss information
     """
-    # with torch.cuda.amp.autocast():
 
     warm_classifier = (il_trainer.cur_warm_stage != -1) and (il_trainer.params['warm_layers'][il_trainer.cur_warm_stage] == 'output')
     with torch.cuda.amp.autocast():
@@ -244,7 +243,7 @@ def train_process(il_trainer : IL_Trainer):
                     for i in range(do_replay_num[replay_iter_num]):
                         data = replay_generator.next()
                         start = time.time()
-                        losses = cal_losses(il_trainer, il_loss, data, is_replay=True)
+                        losses = cal_losses(il_trainer, il_loss, data, is_replay=True, scaler=scaler)
                         if losses == None:
                             continue
                             
@@ -267,7 +266,7 @@ def train_process(il_trainer : IL_Trainer):
                 change_beta(il_trainer, is_replay=True)
                 for iter_num, data in enumerate(il_trainer.dataloader_replay):
                     start = time.time()
-                    losses = cal_losses(il_trainer, il_loss, data, is_replay=True)
+                    losses = cal_losses(il_trainer, il_loss, data, is_replay=True, scaler=scaler)
                     if losses == None:
                         continue
                     end = time.time()